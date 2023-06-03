d45e3e3305538a2595ccae74bd2a3ec81618d9ea
Valentina Vega <mariehvegas@gmail.com>
2023-06-02 20:23:04 -0500

    Project base files and configs - TailwindCSS configutarions and tests - Creation of base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AboutComponent } from './components/about/about.component';
+import { ResultsComponent } from './components/results/results.component';
+import { DetailsComponent } from './components/details/details.component';
+import { AdvancedSearchComponent } from './components/advanced-search/advanced-search.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    AboutComponent,
+    ResultsComponent,
+    DetailsComponent,
+    AdvancedSearchComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
