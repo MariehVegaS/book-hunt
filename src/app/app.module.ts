26e6e2ff1908223af427a8811fa1e0eceb8d172a
Valentina Vega <mariehvegas@gmail.com>
2023-06-03 00:16:35 -0500

    - Navbar created with validation if it's home - Icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { SearchComponent } from './components/search/search.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  showMenu: boolean = false;
+  isHome: boolean =  false;
+  homeRoutes: Array<String> = ['/', '/home']
 
+  constructor(){
+    this.validateRoute();
+  }
+
+  toggleNavbar(){
+    this.showMenu = !this.showMenu;
+  }
+
+  validateRoute(){
+    this.homeRoutes.forEach(route => {
+      if (window.location.pathname === route) {
+        this.isHome = true;
+        return;
+      }
+    });
+  }
 }
